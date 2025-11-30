6b337abe8e8e211ac9d44be7edda0ef30621e222
Shubhangi Gurude <shubhangi.gurude@nihilent.com>
2025-11-30 23:41:34 +0530

    Api Integration

--- src2/app/shopping-cart/models/Item.ts
+++
@@ -1,9 +0,0 @@
-export class Item {
-  constructor(
-    public productId: number,
-    public title: string,
-    public price: number,
-    public quantity: number,
-    public imageUrl: string
-  ) {}
-}
